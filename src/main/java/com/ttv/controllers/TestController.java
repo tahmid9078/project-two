d29790056b4b2a8be3ffc808a271b291aaabb369
tahmid9078 <sakib.9078@gmail.com>
2019-05-09 14:54:43 -0400

    added dependency for tomcat in pom.xml

--- src/main/java/com/ttv/controllers/TestController.java
+++ src/main/java/com/ttv/controllers/TestController.java
@@ -3,16 +3,16 @@ package com.ttv.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ttv.models.Account;
+import com.ttv.models.Role;
 import com.ttv.services.AccountService;
+import com.ttv.services.RoleService;
 
 @RestController
 @RequestMapping("/test")
@@ -21,9 +21,23 @@ public class TestController {
 	@Autowired
 	private AccountService accountService;
 	
+	@Autowired
+	private RoleService roleService;
+	
 	@GetMapping("/accounts")
 	public List<Account> getAllAccount() {
 		return accountService.findAll();
 	}
 	
+//	@PostMapping(path = "/account")
+//	public String addAccount(@RequestBody Account account ) {
+//		account.setRole(roleService.findById((long)50));
+//		accountService.add(account);
+//		return "Ok";
+//	}
+//	@PostMapping(path = "/role")
+//	public String addAccount(@RequestBody Role role ) {
+//		roleService.add(role);
+//		return "ok";
+//	}
 }
