1a8bae37eba26a27feb20d5aa4e0c71ff4526c5a
Thomas Kok <kokthomas7016@gmail.com>
2019-05-14 21:56:19 -0400

    added delete ticket

--- src/main/java/com/ttv/controllers/TicketController.java
+++ src/main/java/com/ttv/controllers/TicketController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,10 @@ public class TicketController {
 	@GetMapping("/account/{id}")
 	public List<Ticket> getAllTicketByAccountId(@PathVariable Long id) {
 		return ticketService.getAllTicketsByAccountId(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void insertTicket(@PathVariable Long id) {
+		ticketService.deleteById(id);
 	}
 }
